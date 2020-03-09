53ec5158215eb7a163a39b862dd85fdd2d351df0
Ashmit Raj <ashmitraj@Ashmits-MacBook-Air.local>
2020-03-09 22:55:58 +0530

    This is the first commit to the respository and it include all the basic crud operations like Create Read Delete and Update. Show page design is done, Edit page design is done and there are so many mire things that have been added

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+
+    def new
+        @article = Article.new
+    end
+
+    def create 
+        @article = Article.new(articles_params)
+        @article.save
+
+        if @article.save
+            flash[:success] = "The article was successfully created"
+            redirect_to article_path(@article)
+        else 
+            render 'new'
+        end
+    end
+
+    def edit 
+        @article = Article.find_by(id: params[:id])
+    end
+    # index
+    def index 
+        
+        @articles  = Article.all
+    end
+
+    # show
+   def show
+       @article = Article.find_by(id: params[:id])
+   end
+
+    def update 
+        @article = Article.find_by(id: params[:id])
+        if @article.update(articles_params)
+            flash[:success] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy 
+        @article = Article.find_by(id: params[:id])
+        @article.destroy
+        redirect_to articles_path
+        flash[:danger] = "The article was successfully destroy"
+    end
+    
+
+
+private 
+    def articles_params
+        params.require(:article).permit(:title,:description)
+
+    end
+
+end
---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+
+    validates :title, presence: true, length: {minimum: 5, maximum: 30}
+    validates_presence_of :description
+    validates_length_of :description, minimum: 25, maximum: 350
+
+      
+
+end
