2c0872627a8ed0e08ef0161a5f74760216e92607
Ashmit Raj <ashmitraj@Ashmits-MacBook-Air.local>
2020-03-10 10:34:57 +0530

    All the features added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
-    validates :title, presence: true, length: {minimum: 5, maximum: 30}
+    validates :title, presence: true, length: {minimum: 5, maximum: 100}
     validates_presence_of :description
     validates_length_of :description, minimum: 25, maximum: 350
 
